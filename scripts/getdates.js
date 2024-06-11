26529c9f1613a7c8e2d55d3939aff78ba2204d18
happylomas <prince56ebere@gmail.com>
2024-06-10 19:05:24 -0500

    formlink

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,22 @@
 
 
-const mainnav = document.querySelector(".navigation");
-const hambutton = document.querySelector("#menu");
+// const mainnav = document.querySelector(".navigation");
+// const hambutton = document.querySelector("#menu");
 
 
-hambutton.addEventListener("click", () => {
-    mainnav.classList.toggle("show");
-    hambutton.classList.toggle("show");
-});
+// hambutton.addEventListener("click", () => {
+//     mainnav.classList.toggle("show");
+//     hambutton.classList.toggle("show");
+// });
+
+
+
+function hambutton() {
+    let x = document.querySelector(".navigation");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "flex";
+    }
+};

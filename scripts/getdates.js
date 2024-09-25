1d8d2054b978fadde38c6f6686a257ea760167b8
happylomas <prince56ebere@gmail.com>
2024-09-25 18:28:29 -0500

    media query activity

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,12 +11,20 @@
 
 
 
-function hambutton() {
-    let x = document.querySelector(".navigation");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "flex";
-    }
-};
+// function hambutton() {
+//     let x = document.querySelector(".navigation");
+//     if (x.style.display === "flex") {
+//         x.style.display = "none";
+//     }
+//     else {
+//         x.style.display = "flex";
+//     }
+// };
+
+
+let lastmodify = document.lastModified;
+let ModifiedElement = document.getElementById("lastModified")
+if (ModifiedElement) {
+    ModifiedElement.textContent = `Last Modification: ${lastmodify}`
+
+}
